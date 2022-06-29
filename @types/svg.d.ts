1ce03fb86379e7125630676654e68f4572134796
Tesseract <tensai3355@gmail.com>
2022-06-29 23:52:22 +0900

    :label: svg 모듈 타입 변경

--- @types/svg.d.ts
+++ @types/svg.d.ts
@@ -1,4 +1,4 @@
 declare module '*.svg' {
-  const value: React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
   export default value;
 }
