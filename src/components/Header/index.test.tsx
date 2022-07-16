18b0c3ff0b9e235b53454c1c688c6c8fd9302000
Tesseract <tensai3355@gmail.com>
2022-07-16 14:20:44 +0900

    :fire: localStorage mock 삭제

--- src/components/Header/index.test.tsx
+++ src/components/Header/index.test.tsx
@@ -1,10 +1,8 @@
 import App from '@src/App';
 import { render, fireEvent } from '@testing-library/react';
-import mockLocalStoarge from 'lib/jest/mock/localStorage';
 
 describe('useMode Hook', () => {
   it('localStorage', () => {
-    mockLocalStoarge();
     window.localStorage.setItem('themeMode', 'DARK');
     const { getByRole } = render(<App />);
     const button = getByRole('switch');
